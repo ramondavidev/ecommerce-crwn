bf8a6e1520a5c6e36eca130faf152c2936ae5659
Ramon <ramondavi@msn.com>
2023-09-05 22:04:47 -0300

    created cartItem and cart context

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,15 @@
+import { useContext } from 'react';
+
 import Button from '../button/button.component';
+import { CartContext } from '../../contexts/cart.contex';
 import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
+
     return (
         <div className='product-card-container'>
             <img src={imageUrl} alt={name} />
@@ -14,7 +21,7 @@ const ProductCard = ({ product }) => {
                     {price}
                 </span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
         </div>
     )
 }
